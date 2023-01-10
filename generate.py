b735f8db02036e3e4fb89f00b73456fe496e2f08
Fajar Maulana Kadir <67577504+norlaklep@users.noreply.github.com>
2023-01-10 20:47:00 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -10,25 +10,5 @@ def random_number():
         name = 'rand' + str(n) + '.dat'
         np.savetxt('data/'+name, array, fmt=['%i'])
 
-def reversed_number():
-    for x in size:
-        number = []
-
-        for n in range(x, 0, -1):
-            number.append(n)
-
-        name = 'reversed' + str(x) + '.dat'
-        np.savetxt('data/'+name, number, fmt=['%i'])
-
-def equal_number():
-    for x in size:
-        number = []
-        for _ in range(x):
-            number.append(5)
-
-        name = 'equal' + str(x) + '.dat'
-        np.savetxt('data/' + name, number, fmt=['%i'])
 
 random_number()
-reversed_number()
-equal_number()
